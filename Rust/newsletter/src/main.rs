386e6daae90e2fc35edae54257f802109ae21c8f
mateovelilla <teovelilla@gmail.com>
2024-06-15 12:28:17 -0500

    feat: created new feature to random port ✨

--- Rust/newsletter/src/main.rs
+++ Rust/newsletter/src/main.rs
@@ -1,6 +1,7 @@
 use newsletter::run;
-
+use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address"); // When we define a connection with a port in 0 then the S.O search a port enable to create the connection
+    run(listener)?.await
 }
--- Rust/newsletter/tests/health_check.rs
+++ Rust/newsletter/tests/health_check.rs
@@ -1,15 +1,19 @@
 // Integration tests
-fn spawn_app(){
-    let server = newsletter::run().expect("Failed to bind adress");
+use std::net::TcpListener;
+fn spawn_app() -> String{
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address"); // When we define a connection with a port in 0 then the S.O search a port enable to create the connection
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter::run(listener).expect("Failed to bind adress"); // When we define a connection with a port in 0 then the S.O search a port enable to create the connection
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
 #[tokio::test]
 async fn health_check_works(){
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     //Act
-    let response = client.get("http://127.0.0.1:8000/health_check")
+    let response = client.get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
